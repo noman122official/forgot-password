ac623b5649fe1ba8378b0a4aede57a060d79219f
noman122official <67466365+noman122official@users.noreply.github.com>
2021-09-22 21:29:23 +0530

    added vectors & text input fields according to ui

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,79 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from "next/head";import Image from "next/image";
+
+import styled, { css } from "styled-components";
+
+import skilzenlogo from "../public/skilzenlogo.png";
+
+import leftvector from "../public/leftvector.svg";
+import rightvector from "../public/rightvector.svg";
+
+import passwordImage from "../public/forgotpass1.svg";
+
+
+const StyledLink = styled.a`
+  color: #f26a7e;
+`;
 
 export default function Home() {
   return (
-    <div>
-      {/* <div className={styles.element1}>
-        <Image
-          src="/element1.svg"
-          alt="Vercel Logo"
-          width={449.76}
-          height={439}
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap"
+          rel="stylesheet"
         />
-      </div>
-      <div className={styles.element2}>
-        <Image
-          src="/element2.svg"
-          alt="Vercel Logo"
-          width={251.93}
-          height={245.9}
-        />
-      </div> */}
-      <div className={styles.forgotpass}>
-        <Image
-        src="/forgotpass2.svg"
-        width={433}
-        height={317.73}
-        />
-      </div>
-      <div className={styles.boxLeft}>
-
-      </div>
-      <div className={styles.boxRight}>
-
-      </div>
-      <div>
-      <p className={styles.almostDone}>Let Us Help!</p>
-      <p className={styles.passwordReset}>Forgot Password</p>
-      <p className={styles.warningSmall}>Mention your Registered Email/mobile number to get the verification code.</p>
-      <p className={styles.registeredEmail}>Registered Email</p>
-      <input className={styles.emailField}></input>
-      </div>
-    </div>
+      </head>
+      <body>
+        <div className="leftvector">
+          <Image src={leftvector} />
+        </div>
+        <div className="rightvector" style={{ transform: "rotate(-60deg)" }}>
+          <Image src={rightvector} />
+        </div>
+        <div className="signup-main">
+          <div className="image-block">
+            <div className="skilzenlogo">
+              <Image src={skilzenlogo} />
+            </div>
+            <div className="signupImage">
+              <Image width={400} height={280} src={passwordImage} />
+            </div>
+          </div>
+          <div className="signin-block">
+            <div className="toptext-signin">
+              <b>Let Us Help!</b>
+            </div>
+            <div className="secondtext">Forgot Password</div>
+            <div className="warnigtext">
+              Mention your Registered Email/mobile number to get the
+              verification code.
+            </div>
+            <div className="email">
+              <span>Registered Email</span>
+              <input placeholder="someone@gmail.com" />
+            </div>
+            <div className="ortext">Or</div>
+            <div className="password">
+              <span>Contact Number</span>
+              <input placeholder="Your Phone Number" />
+            </div>
+            <div className="checkEmail">Check you Email/Text to get the code</div>
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                paddingTop: "15%",
+              }}
+            >
+            
+              <button className="signinbtn">Enter</button>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
