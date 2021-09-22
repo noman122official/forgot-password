6cf9ad2939f18417be4f8d86118e6acc12d9b31c
noman122official <67466365+noman122official@users.noreply.github.com>
2021-09-22 20:24:53 +0530

    updated index.js and home css

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-          <div className={styles.element1}>
-            <Image src="/element1.svg" alt="Vercel Logo" width={449.76} height={439}  />
-          </div>
-          <div className={styles.element2}>
-          <Image src="/element2.svg" alt="Vercel Logo" width={251.93} height={245.9}  />
-        </div>
+      {/* <div className={styles.element1}>
+        <Image
+          src="/element1.svg"
+          alt="Vercel Logo"
+          width={449.76}
+          height={439}
+        />
+      </div>
+      <div className={styles.element2}>
+        <Image
+          src="/element2.svg"
+          alt="Vercel Logo"
+          width={251.93}
+          height={245.9}
+        />
+      </div> */}
+      <div className={styles.forgotpass}>
+        <Image
+        src="/forgotpass2.svg"
+        width={433}
+        height={317.73}
+        />
+      </div>
+      <div className={styles.boxLeft}>
+
+      </div>
+      <div className={styles.boxRight}>
+
+      </div>
+      <div>
+      <p className={styles.almostDone}>Let Us Help!</p>
+      <p className={styles.passwordReset}>Forgot Password</p>
+      <p className={styles.warningSmall}>Mention your Registered Email/mobile number to get the verification code.</p>
+      <p className={styles.registeredEmail}>Registered Email</p>
+      <input className={styles.emailField}></input>
+      </div>
     </div>
-          
-  )
+  );
 }
